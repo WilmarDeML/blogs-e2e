6b1f65b02d4ceb978eb9d79b235dafdccf6c92da
WilmarDeML <carewaz23@hotmail.com>
2025-03-11 15:02:03 -0500

    solve exercise 5.18

--- tests/blogs_app.spec.js
+++ tests/blogs_app.spec.js
@@ -1,7 +1,18 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith, createBlog } = require('./helper')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      },
+      
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -11,5 +22,20 @@ describe('Blog app', () => {
 
     await expect(buttonLogin).toBeVisible()
     await expect(locator).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'mluukkai', 'salainen')
+      await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'mluukkai', 'wrong password')      
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toContainText('invalid username or password')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+    })
   })
 })
